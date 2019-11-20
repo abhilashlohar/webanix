2431e6aa69ee3edf10e42091819499cfb91aa965
Abhilash Lohar <abhilashlohar01@gmail.com>
2019-11-20 23:43:09 +0530

    user module

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('years','YearController');
 Route::resource('courses','CourseController');
 Route::resource('semesters','SemesterController');
+Route::resource('users','UserController');
+
