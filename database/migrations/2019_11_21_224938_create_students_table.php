efe3aeb511711ea8258d00c7e6ad06774fd90413
Abhilash Lohar <abhilashlohar01@gmail.com>
2019-11-21 23:20:11 +0530

    student module stage 2

---
+++ database/migrations/2019_11_21_224938_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('enrollment');
+            $table->string('name');
+            $table->string('father_name')->nullable();
+            $table->string('mother_name')->nullable();
+            $table->date('dob')->nullable();
+            $table->uuid('course_id');
+            $table->uuid('stream_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
