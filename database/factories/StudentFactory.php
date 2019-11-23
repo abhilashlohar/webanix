280ab7beec6b2aa3add2d353e118532711444597
Ubuntu <ubuntu@ip-172-31-44-56.ap-south-1.compute.internal>
2019-11-23 23:02:28 +0000

    sdfdsf

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -12,8 +12,8 @@ $factory->define(Student::class, function (Faker $faker) {
         'father_name' => $faker->name,
         'mother_name' => $faker->name,
         'dob' => $faker->date($format = 'Y-m-d', $max = 'now'),
-        'course_id' => '464e33d7-1e9d-4e7b-881b-5298e1e86f1f',
-        'stream_id' => '44620e94-2e46-46fb-b0a0-0da4948e9117',
+        'course_id' => '0b5f839b-b481-4be0-83c0-b72799d3efc1',
+        'stream_id' => null,
 
     ];
 });
