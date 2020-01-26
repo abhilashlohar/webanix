9ddde9339e45228f11b8944e5b2e29f7c2e04e76
Ubuntu <ubuntu@ip-172-31-44-56.ap-south-1.compute.internal>
2020-01-26 14:30:07 +0000

    sdfdsf

--- resources/views/students/report.blade.php
+++ resources/views/students/report.blade.php
@@ -19,6 +19,8 @@
     .sidebar{
         disply:none;
     }
+    tbody {display: table-header-group;}
+
 }
 </style>
 @section('content')
@@ -31,7 +33,7 @@
                 <div class="form-group">
                     <label>Year</label>
                     <select name="year_id" id="year_id" class="form-control">
-                        <option value="">---Select Year---</option>
+                        <option value="">---All years---</option>
                         @foreach ($years as $year)
                             <option 
                             value="{{ $year->id }}"
@@ -43,118 +45,125 @@
             
             <div class="col-md-3">
               <button type="submit" class="btn btn-primary" style="margin-top: 12%;">Search</button>
-              <button type="button" onclick="myFunction()" class="btn btn-primary" style="margin-top: 12%;">Ptint</button>
+              <button type="button" onclick="myFunction()" class="btn btn-success" style="margin-top: 12%;">Print</button>
             </div>
            
         </div>
     </form>
 </div>
 </div>
-<div id="section-to-print">
-<div style="width:100%;text-align:center;"><h3>Student Statistics</h3></div><hr/>
-<table width="100%" border="1">
-    <tr>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-              <span style="font-size: 26px;color: #4D384B;">{{ $student }}</span><br>
-              <span style="font-size: 16px;color: #4D384B;">Total Students</span>
-            </div>
-        </td>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                <span style="font-size: 26px;color: #4D384B;">{{ $marksheets }}</span><br>
-                <span style="font-size: 16px;color: #4D384B;">Total Marksheets</span>
-            </div>
-        </td>
-    </tr>
-</table>
-<h4>MarkSheet by Result</h4>
-<table width="100%" border="1">
-    @foreach($results as $result)
-    <tr>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                  <span style="font-size: 16px;color: #4D384B;">{{ $result->result }}</span>
-            </div>
-        </td>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                <span style="font-size: 16px;color: #4D384B;">{{ $result->total }}</span>
-            </div>
-        </td>
-    </tr>
-    @endforeach
-</table>
-<h4>MarkSheet by Session</h4>
-<table width="100%" border="1">
-    @foreach($sessions as $session)
-    <tr>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                  <span style="font-size: 16px;color: #4D384B;">{{ $session->session }}</span>
-            </div>
-        </td>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                <span style="font-size: 16px;color: #4D384B;">{{ $session->total }}</span>
-            </div>
-        </td>
-    </tr>
-    @endforeach
-</table>
-<h4>MarkSheet by Course</h4>
-<table width="100%" border="1">
-    @foreach ($course_wise_students as $course_stu)
-    <tr>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                  <span style="font-size: 16px;color: #4D384B;">{{ $course_stu->course->name }}</span>
-            </div>
-        </td>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                <span style="font-size: 16px;color: #4D384B;">{{ $course_stu->total }}</span>
-            </div>
-        </td>
-    </tr>
-    @endforeach
-</table>
-@if($request->year_id=='')
-<h4>MarkSheet by Year</h4>
-<table width="100%" border="1">
-    @foreach ($year_wise_students as $yr_stu)
-    <tr>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                  <span style="font-size: 16px;color: #4D384B;">{{ $yr_stu->year->name }}</span>
-            </div>
-        </td>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                <span style="font-size: 16px;color: #4D384B;">{{ $yr_stu->total }}</span>
-            </div>
-        </td>
-    </tr>
-    @endforeach
-</table>
-<h4>MarkSheet by Stream</h4>
-<table width="100%" border="1">
-     @foreach ($stream_wise_students as $stram_stu)
-    <tr>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                  <span style="font-size: 16px;color: #4D384B;">{{ $stram_stu->stream->name }}</span>
-            </div>
-        </td>
-        <td width="50%">
-            <div align="center" style="background-color: #FFF;border-radius: 5px;border: solid 1px #cccccc;">
-                <span style="font-size: 16px;color: #4D384B;">{{ $stram_stu->total }}</span>
-            </div>
-        </td>
-    </tr>
-    @endforeach
-</table>
- @endif
+<div id="section-to-print" style="background-color: #FFF;padding: 10px 10px;">
+    <div style="width:100%;text-align:center;"><h3>Student Statistics</h3></div><hr/>
+        <table class="table table-bordered">
+            <tr>
+                <td width="50%" align="center">
+                    <div >
+                      <span style="font-size: 26px;color: #4D384B;">{{ $student }}</span><br>
+                      <span style="font-size: 16px;color: #4D384B;">Total Students</span>
+                    </div>
+                </td>
+                <td width="50%" align="center">
+                    <div >
+                        <span style="font-size: 26px;color: #4D384B;">{{ $marksheets }}</span><br>
+                        <span style="font-size: 16px;color: #4D384B;">Total Marksheets</span>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        
+        <h4>Marksheets by result</h4>
+        <table class="table table-bordered table-sm">
+            @foreach($results as $result)
+            <tr>
+                <td width="50%">
+                    <div >
+                          <span style="font-size: 16px;color: #4D384B;">{{ $result->result }}</span>
+                    </div>
+                </td>
+                <td width="50%">
+                    <div >
+                        <span style="font-size: 16px;color: #4D384B;">{{ $result->total }}</span>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+
+        <h4>Marksheets by session</h4>
+        <table class="table table-bordered table-sm">
+            @foreach($sessions as $session)
+            <tr>
+                <td width="50%">
+                    <div >
+                          <span style="font-size: 16px;color: #4D384B;">{{ $session->session }}</span>
+                    </div>
+                </td>
+                <td width="50%">
+                    <div >
+                        <span style="font-size: 16px;color: #4D384B;">{{ $session->total }}</span>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+
+        <h4>Marksheets by course</h4>
+        <table class="table table-bordered table-sm">
+            @foreach ($course_wise_students as $course_stu)
+            <tr>
+                <td width="50%">
+                    <div >
+                          <span style="font-size: 16px;color: #4D384B;">{{ $course_stu->course->name }}</span>
+                    </div>
+                </td>
+                <td width="50%">
+                    <div >
+                        <span style="font-size: 16px;color: #4D384B;">{{ $course_stu->total }}</span>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+
+        <h4>Marksheets by stream</h4>
+        <table class="table table-bordered table-sm">
+            <tbody>
+                @foreach ($stream_wise_students as $stram_stu)
+                <tr>
+                    <td width="50%">
+                        <div >
+                              <span style="font-size: 16px;color: #4D384B;">{{ $stram_stu->stream->name }}</span>
+                        </div>
+                    </td>
+                    <td width="50%">
+                        <div >
+                            <span style="font-size: 16px;color: #4D384B;">{{ $stram_stu->total }}</span>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        @if($request->year_id=='')
+        <h4>Marksheets by year</h4>
+        <table class="table table-bordered table-sm">
+            @foreach ($year_wise_students as $yr_stu)
+            <tr>
+                <td width="50%">
+                    <div >
+                          <span style="font-size: 16px;color: #4D384B;">{{ $yr_stu->year->name }}</span>
+                    </div>
+                </td>
+                <td width="50%">
+                    <div >
+                        <span style="font-size: 16px;color: #4D384B;">{{ $yr_stu->total }}</span>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+        @endif
 </div>
 @endsection
 <script>
